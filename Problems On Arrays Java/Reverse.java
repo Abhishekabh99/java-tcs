4637095924cbc85218ead724a24785f4dc1310da
abhishek <abhishekabh99@gmail.com>
2024-06-18 16:31:41 +0530

    reverse array

--- Problems On Arrays Java/Reverse.java
+++ Problems On Arrays Java/Reverse.java
@@ -1,8 +1,13 @@
-import java.util.Scanner;
+import java.util.*;
 
 class Reverse {
-public static void main() {
+public static void main(String[] args) {
 int[] arr = input_array();
+System.out.println("below is reversed array :");
+Reverse ob = new Reverse();
+ob.reverseUsingRecursion(arr, 0, arr.length-1);
+ob.display(arr);
+
 }
 static int[] input_array() {
     System.out.println("Enter size of array :");
@@ -15,5 +20,53 @@ static int[] input_array() {
     }
     return arr;
 }
+//method 1 make a new array
+int[] reverse(int[] arr) {
+    int s = arr.length;
+    int [] arr1 = new int[s];
+    for(int i=arr.length-1;i>=0;i--) {
+arr1[s-i-1] = arr[i];
+    }
+    return arr1;
+}
+
+//method 2 using two pointers
+int[] reverseUsingTwoPointer(int[] arr) {
+int s = 0;
+int e = arr.length-1;
+while(s<e) {
+    int temp = arr[s];
+    arr[s] = arr[e];
+    arr[e] = temp;
+    s++;
+    e--;
+}
+return arr;
+}
+void display(int[] arr) {
+    int i=0;
+    while(i<arr.length) {
+        System.out.println(arr[i]);
+        i++;
+    }
+}
+
+//method 3 using recursion
+int[] reverseUsingRecursion(int[] arr,int s,int e) {
+if(s<e) {
+    int temp = arr[s];
+    arr[s] = arr[e];
+    arr[e] = temp;
+    reverseUsingRecursion(arr, s+1, e-1);
+}
+return arr;
+}
+
+//method 4 using java package library
+int[] reverseUsingLibrary(int[] arr) {
+    Collections.reverse((Arrays.asList(arr)));
+    return arr;
+}
+
 
 }
